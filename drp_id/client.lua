8250ae07142a15249deee2929d46fca7042d227a
Darkzy <tomames360@hotmail.co.uk>
2020-01-12 23:36:16 +0000

    Revert "CFX.RE Patch Fixed. New Install Instruction ETC."
    This reverts commit 0a6a6bc607eefbb4caeba078ace00b968427cfe4.

--- drp_id/client.lua
+++ drp_id/client.lua
@@ -78,15 +78,14 @@ AddEventHandler("DRP_ID:LoadSelectedCharacter", function(ped, spawn, spawnInHote
 	---------------------------------------------------------------------------
 	TriggerEvent("DRP_ID:StopSkyCamera")
 	TriggerEvent("DRP_ID:StopCreatorCamera") -- If you are using this system
-	OnCharacterLoadEvents(spawnInHotel)
-	-- TriggerServerEvent("DRP_Clothing:FirstSpawn") -- If Clothing Is Installed
-	-- TriggerServerEvent("DRP_Death:GetDeathStatus") -- Checks if they died before, so no leaving and joining back dead
-	-- TriggerServerEvent("DRP_Garages:CheckLockPicking")
-	-- TriggerServerEvent("DRP_Auction:GetVehicleList")
-	-- TriggerServerEvent("DRP_Housing:InitializePlayer", spawnInHotel)
-	-- TriggerServerEvent("DRP_Farming:SyncData") -- If Farming Is Installed
-	-- TriggerServerEvent("DRP_Doors:StartSync") -- If Doors is Installed
-	-- TriggerServerEvent("DRP_Tattoos:GetTattoos") -- If Tattoos is Installed
+	TriggerServerEvent("DRP_Clothing:FirstSpawn") -- If Clothing Is Installed
+	TriggerServerEvent("DRP_Death:GetDeathStatus") -- Checks if they died before, so no leaving and joining back dead
+	TriggerServerEvent("DRP_Garages:CheckLockPicking")
+	TriggerServerEvent("DRP_Auction:GetVehicleList")
+	TriggerServerEvent("DRP_Housing:InitializePlayer", spawnInHotel)
+	TriggerServerEvent("DRP_Farming:SyncData") -- If Farming Is Installed
+	TriggerServerEvent("DRP_Doors:StartSync") -- If Doors is Installed
+	TriggerServerEvent("DRP_Tattoos:GetTattoos") -- If Tattoos is Installed
 	---------------------------------------------------------------------------
 end)
 ---------------------------------------------------------------------------
