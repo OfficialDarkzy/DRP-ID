428d795259bc5b5de0cdf853cb478d0d790b0597
Shredxt <47064656+shredxt@users.noreply.github.com>
2020-04-01 14:58:46 +0100

    drp_progressBars + car was example

--- drp_garages/carwash.lua
+++ drp_garages/carwash.lua
@@ -1,3 +1,4 @@
+local washing = false
 local carWashLocations = {
 	{name = "Carwash", id = 100, colour = 2, blipSize = 0.7, x = 24.81,  y = -1391.87, z = 28.33},
 	{name = "Carwash", id = 100, colour = 2, blipSize = 0.7, x = 2524.42,  y = 4195.39, z = 38.96}
@@ -18,19 +19,28 @@ if DRPGarages.Carwash then
 		while true do
 			local sleepTime = 1000
 			for a = 1, #carWashLocations do
-			local ped = GetPlayerPed(PlayerId())
-			local pedPos = GetEntityCoords(ped, false)
-			local distance = Vdist(pedPos.x, pedPos.y, pedPos.z, carWashLocations[a].x, carWashLocations[a].y, carWashLocations[a].z)
-			if distance <= 25.0 then
-				sleepTime = 5
-				DrawMarker(1, carWashLocations[a].x, carWashLocations[a].y, carWashLocations[a].z, 0, 0, 0, 0, 0, 0, 5.001, 5.0001, 0.5001, 0, 155, 255, 200, 0, 0, 0, 0)
+				local ped = PlayerPedId()
+				local pedPos = GetEntityCoords(ped)
+				local distance = Vdist(pedPos.x, pedPos.y, pedPos.z, carWashLocations[a].x, carWashLocations[a].y, carWashLocations[a].z)
+				if distance <= 20.0 then
+					sleepTime = 5
+					DrawMarker(1, carWashLocations[a].x, carWashLocations[a].y, carWashLocations[a].z, 0, 0, 0, 0, 0, 0, 5.001, 5.0001, 0.5001, 0, 155, 255, 200, 0, 0, 0, 0)
 					if distance <= 5.0 then 
 						local pedVehicle = GetVehiclePedIsIn(ped)
+						local driver = GetPedInVehicleSeat(pedVehicle, -1)
 						local carWashCost = 25
-						if IsPedSittingInAnyVehicle( ped ) then
-							drawTxt3D(carWashLocations[a].x, carWashLocations[a].y, carWashLocations[a].z + 0.9, 'Press ~s~[~b~E~s~] to clean your car!')
-							if IsControlJustPressed(1, 86) then	
-								TriggerServerEvent("DRP_CarWash:CheckMoney", carWashCost)
+						if IsPedSittingInAnyVehicle(ped) then
+							if driver == ped and not washing then
+								drawTxt3D(carWashLocations[a].x, carWashLocations[a].y, carWashLocations[a].z + 0.9, 'Press ~s~[~b~ENTER~s~] to clean your car!')
+								if IsControlJustPressed(1, 18) then
+									washing = true
+									FreezeEntityPosition(pedVehicle, true)
+									exports['drp_progressBars']:startUI(15000, "Washing Car...")
+									Wait(15000)
+									TriggerServerEvent("DRP_CarWash:CheckMoney", carWashCost)
+									FreezeEntityPosition(pedVehicle, false)
+									washing = false
+								end
 							end
 						end
 					end
@@ -43,7 +53,8 @@ end
 
 RegisterNetEvent("DRP_CarWash:YesCleanCar")
 AddEventHandler("DRP_CarWash:YesCleanCar", function()
-	local ped = GetPlayerPed(PlayerId())
-	WashDecalsFromVehicle(GetVehiclePedIsUsing(ped, 1.0))
-	SetVehicleDirtLevel(GetVehiclePedIsUsing(ped))
+	local ped = PlayerPedId()
+	local vehicle = GetVehiclePedIsIn(ped)
+	WashDecalsFromVehicle(vehicle, 1.0)
+	SetVehicleDirtLevel(vehicle, 0.0)
 end)
