bda6b90e2a7f9b5ad1a898d9aaadee292dd7dadb
EnVyyyyy <envyart12@gmail.com>
2020-04-21 22:47:05 +0200

    Changed veh lock animation + fix for store veh

--- drp_garages/client.lua
+++ drp_garages/client.lua
@@ -215,14 +215,14 @@ function storeVehicle()
 	local garage = GetClosestVehicle(garageSelected.x, garageSelected.y, garageSelected.z, 25.0, 0, 70)
 	local plate = GetVehicleNumberPlateText(garage)
 
-		if AreAnyVehicleSeatsFree(garage) then
-			if DoesEntityExist(garage) then
+		if DoesEntityExist(garage) then
+			if AreAnyVehicleSeatsFree(garage) then
 				TriggerServerEvent("DRP_Garages:RequestStoreVehicle", plate)
 			else
-				drawNotification("Cannot find any vehicles near you!")
+				drawNotification("Vehicle not empty!")
 			end
 		else
-			drawNotification("Vehicle not empty!")
+			drawNotification("Cannot find any vehicles near you!")
 		end
 	end)
 end
