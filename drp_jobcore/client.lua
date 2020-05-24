86e1cf08c022ae12c9a01014970cdbfca0d06d92
Darkzy <tomames360@hotmail.co.uk>
2020-05-24 01:16:02 +0100

    yeet

--- drp_jobcore/client.lua
+++ drp_jobcore/client.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function()
     Citizen.Wait(10000)
     while true do
-        if exports["drp_id"]:SpawnedInAndLoaded() == true then
+        if exports["drp_id"]:SpawnedInAndLoaded() then
             TriggerServerEvent("DRP_JobCore:Salary")
         end
         Citizen.Wait(JobsCoreConfig.SalaryRecieveTimer)
